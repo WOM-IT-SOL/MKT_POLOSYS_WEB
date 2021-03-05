33c061e4901ebc089c3130e53f89537875ceca79
Ryan Maulana Putra <ryputra@wom.co.id>
2021-03-05 15:42:58 +0700

    Add project files.

---
+++ MKT_POLOSYS_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MKT_POLOSYS_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
