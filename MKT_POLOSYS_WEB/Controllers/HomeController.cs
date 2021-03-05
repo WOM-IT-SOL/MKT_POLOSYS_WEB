e4276b35594a7c9a5c4d90956c30daea1400f980
Ryan Maulana Putra <ryputra@wom.co.id>
2021-03-05 16:18:41 +0700

    add function hello

--- MKT_POLOSYS_WEB/Controllers/HomeController.cs
+++ MKT_POLOSYS_WEB/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace MKT_POLOSYS_WEB.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public void hello()
+        {
+            Console.WriteLine("hello");
+        }
     }
 }
